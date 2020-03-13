40dc9c8fd4249b3c4f00e0707d8b07daed02b5ff
sunil1711 <35965270+sunil1711@users.noreply.github.com>
2020-03-13 13:04:45 +0530

    nk
    kknk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  Login  from './components/login';
+import { connect} from 'react-redux'
+import { Store } from 'redux';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router';
+import {redux}  from 'react-redux';
+import  configureStore from  './store/store';
+
+
+const store = configureStore();
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Switch>
+    <Route path="/" components={Login}/>
+
+    </Switch>
+    </Provider>
   );
 }
 
---
+++ src/components/login.js
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+function Login(){
+
+
+
+    function Submit(){
+   alert("hh")
+
+    }
+
+return (
+
+     <div>
+         <h2>Login</h2>
+
+    <input type="text"/>
+    <br/>
+    <input type="password"/>
+    <br/>
+    <button onClick={Submit}    >Submit</button>
+
+    </div>
+
+
+)
+
+
+
+}
+
+export default Login
